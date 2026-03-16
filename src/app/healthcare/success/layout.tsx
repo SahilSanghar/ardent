151bbdc28c3ccfd4318e2deabbe89ac56003a8bd
sahil sanghar <sangharsahil546@gmail.com>
2026-03-16 16:24:57 +0530

    ...

--- src/app/healthcare/success/layout.tsx
+++ src/app/healthcare/success/layout.tsx
@@ -1,57 +1,12 @@
-// import Script from "next/script";
-
 export const metadata = {
   title: "Success",
   description: "Successfully submitted the form",
 };
 
-export default function RootLayout({
+export default function SuccessLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      {/* Google Analytics Script */}
-
-      {/* <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=AW-17389056295"
-      ></Script> */}
-
-      <head>
-        {/* <Script id="google-analytics-success">
-          {`
-  gtag('event', 'conversion', {
-      'send_to': 'AW-17389056295/4JMxCJzAy_caEKfi3-NA',
-      'value': 1.0,
-      'currency': 'INR'
-  });
-  `}
-        </Script> */}
-
-        {/* <Script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=AW-17389056295"
-        ></Script>{" "}
-
-        <Script id="google-analytics-init">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'AW-17389056295');
-          `}
-        </Script>
-
-        <Script id="google-analytics-success-healthcare">
-          {`
-            gtag('event', 'conversion', {'send_to': 'AW-17389056295/qd-gCOKRopMbEKfi3-NA'});
-          `}
-        </Script> */}
-      </head>
-
-      <body>{children}</body>
-    </html>
-  );
+  return <>{children}</>;
 }
