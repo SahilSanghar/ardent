d9d06164dfefa24e92e5d2ae53cf4d706277f7d4
sahil sanghar <sangharsahil546@gmail.com>
2026-04-07 21:25:44 +0530

    ...

---
+++ src/app/api/ArdentPrAgency/route.ts
@@ -0,0 +1,73 @@
+import nodemailer from "nodemailer";
+
+import { NextRequest, NextResponse } from "next/server";
+
+// Create a transporter configuration
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST || "",
+  port: parseInt(process.env.SMTP_PORT || "587"),
+  secure: process.env.SMTP_SECURE === "true" ? true : false,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
+
+export async function POST(request: NextRequest) {
+  const {
+  firstname,
+  lastname,
+  companyname,
+  designation,
+  phone,
+  email,
+  message
+} = await request.json();
+
+  try {
+    // Send email
+    await transporter.sendMail({
+      from: `"ArdentCo" <${process.env.SMTP_FROM}>`,
+      to: "aishwarya.anup@ardentco.in", // Where you want to receive emails
+      // to: "famousforgames@gmail.com",
+      replyTo: email, // Set reply-to as sender's email
+      subject: `Ardent PR Agency Form Submitted by ${email}`,
+      text: `
+          Name: ${firstname + " " + lastname}
+  Company: ${companyname}
+  Designation: ${designation}
+          Phone: ${phone}
+          Email: ${email}
+          Message: ${message}
+        `,
+      html: `
+          <h1>Ardent PR Agency Form</h1>
+          <p><strong>Name:</strong> ${firstname} ${lastname}</p>
+  <p><strong>Company:</strong> ${companyname}</p>
+  <p><strong>Designation:</strong> ${designation}</p>
+          <p><strong>Phone:</strong> ${phone}</p>
+          <p><strong>Email:</strong> ${email}</p>
+          <p><strong>Message:</strong> ${message}</p>
+        `,
+    });
+
+    return NextResponse.json(
+      { message: "Email sent successfully" },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    console.error("Email send error:", error);
+    // res.status(500).json({
+    //   message: "Failed to send email",
+    //   error: error.message,
+    // });
+    return NextResponse.json(
+      { message: "Failed to send email" },
+      {
+        status: 500,
+      }
+    );
+  }
+}
